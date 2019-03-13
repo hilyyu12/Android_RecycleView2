7b12f06f7ea68c89b680aac7445ad4e323b7d8ec
abiyyu hilmi <abiyyumi1112@gmail.com>
2019-03-13 20:38:47 +0700

    first commit

---
+++ app/src/main/java/model/hotel.java
@@ -0,0 +1,14 @@
+package model;
+import android.graphics.drawable.Drawable;
+
+
+public class hotel {
+    public String judul, deskripsi;
+    public Drawable foto;
+
+    public hotel(String judul, String deskripsi, Drawable foto) {
+        this.judul = judul;
+        this.deskripsi = deskripsi;
+        this.foto = foto;
+    }
+}
